9af82455555a4f75d71c3e3140d2fc1d5754ac42
Zenith Slayer <miguellurk@gmail.com>
2025-03-26 19:00:28 -0300

    fixed some animation problems

--- scripts/objRefTable.js
+++ scripts/objRefTable.js
@@ -3,53 +3,47 @@ self.C3_GetObjectRefTable = function () {
 	return [
 		C3.Plugins.Sprite,
 		C3.Behaviors.Turret,
-		C3.Behaviors.MoveTo,
+		C3.Behaviors.Orbit,
 		C3.Plugins.Mouse,
 		C3.Behaviors.Car,
-		C3.Plugins.System.Cnds.OnLayoutStart,
-		C3.Plugins.Sprite.Cnds.OnCollision,
-		C3.Plugins.System.Cnds.ForEach,
-		C3.Plugins.Sprite.Acts.Destroy,
-		C3.Behaviors.Car.Acts.SetMaxSpeed,
-		C3.Behaviors.Car.Acts.SetAcceleration,
-		C3.Plugins.System.Acts.Wait,
-		C3.Behaviors.Car.Acts.SimulateControl,
+		C3.Behaviors.Turret.Cnds.IsEnabled,
+		C3.Behaviors.Turret.Acts.AcquireTarget,
 		C3.Plugins.Mouse.Cnds.OnClick,
+		C3.Plugins.Sprite.Acts.Destroy,
 		C3.Plugins.System.Acts.WaitForPreviousActions,
 		C3.Plugins.System.Acts.CreateObject,
 		C3.Plugins.Mouse.Exps.X,
 		C3.Plugins.Mouse.Exps.Y,
-		C3.Behaviors.Turret.Cnds.IsEnabled,
-		C3.Behaviors.Turret.Acts.AcquireTarget,
 		C3.Behaviors.Turret.Cnds.OnShoot,
 		C3.Plugins.Sprite.Exps.X,
 		C3.Plugins.Sprite.Exps.Y,
 		C3.Plugins.Sprite.Acts.SetTowardPosition,
-		C3.Behaviors.Turret.Acts.SetEnabled,
-		C3.Plugins.System.Cnds.While,
-		C3.Plugins.System.Cnds.CompareVar,
-		C3.Plugins.System.Acts.SubVar,
-		C3.Behaviors.MoveTo.Acts.MoveToObject,
-		C3.Plugins.Sprite.Cnds.OnAnimFinished,
-		C3.Plugins.Sprite.Acts.SetAnim
+		C3.Plugins.System.Cnds.EveryTick,
+		C3.Behaviors.Car.Acts.SimulateControl,
+		C3.Plugins.Sprite.Acts.SetScale,
+		C3.Plugins.Sprite.Cnds.OnCollision,
+		C3.Plugins.System.Cnds.ForEach,
+		C3.Behaviors.Orbit.Acts.Pin,
+		C3.Behaviors.Orbit.Acts.SetSpeed,
+		C3.Plugins.System.Exps.random
 	];
 };
 self.C3_JsPropNameTable = [
 	{Turret: 0},
-	{MC_Ship: 0},
-	{MoveTo: 0},
-	{Enemy: 0},
-	{Mouse: 0},
-	{Aim: 0},
-	{Car: 0},
 	{Sprite: 0},
-	{EnemiesThisRound: 0}
+	{Orbit: 0},
+	{Sprite2: 0},
+	{Sprite3: 0},
+	{Mouse: 0},
+	{Car: 0},
+	{Sprite4: 0},
+	{RoundEnemies: 0}
 ];
 
 self.InstanceType = {
-	MC_Ship: class extends self.ISpriteInstance {},
-	Enemy: class extends self.ISpriteInstance {},
+	Sprite: class extends self.ISpriteInstance {},
+	Sprite2: class extends self.ISpriteInstance {},
+	Sprite3: class extends self.ISpriteInstance {},
 	Mouse: class extends self.IInstance {},
-	Aim: class extends self.ISpriteInstance {},
-	Sprite: class extends self.ISpriteInstance {}
+	Sprite4: class extends self.ISpriteInstance {}
 }
